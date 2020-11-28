3cf3c44ce7c776d10a634c2193ab0512b850cadf
mikiyasayele <mymikiaa11@gmail.com>
2020-11-28 18:12:26 +0300

    made custom hero section just for mobile users

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -61,6 +61,9 @@ class HomePage extends React.Component{
             <Box className='hero-section-subtitle'>You've had the rest, come try the best!</Box>
           </Typography>
           <Button endIcon={<ChevronRightIcon/>} size='small' style={{margin:'.5em'}} color='secondary'>Try the best</Button>
+          
+            <Image className='hero-section-mobile-image' cloudName='mikiyas' publicId='c-item2_qewdgb'  secure="true"/>
+
           <div className='hero-section-grid'>
             {/* <img src='https://instagram.fadd1-1.fna.fbcdn.net/v/t51.2885-15/e35/c59.0.1122.1122a/s480x480/97237411_248415766235891_5659653975019641466_n.jpg?_nc_ht=instagram.fadd1-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=idrsKOxco2gAX_YEahg&tp=1&oh=c877491441a7ade3d65bf08f13c6f7fc&oe=5FEC9883' className='hero-section-grid-img-short-2'/> */}
             <Image  cloudName='mikiyas' className='hero-section-grid-img-short-2' publicId='97237411_248415766235891_5659653975019641466_n_sxwi4c' secure="true"/>
